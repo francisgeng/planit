e1693edefe246049120c179d20427e0e94cd57f3
planit <planit@tanjeffreyz.github.io>
2022-11-02 04:41:01 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,577 @@
 assignments = {
-  "INDENG 120 - Fall 2022": []
+  "CS 182/282A": [
+    {
+      "title": "HW 0 (Written)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-07T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2205799/submissions/132285985",
+      "submitted": true
+    },
+    {
+      "title": "HW 0 (Code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-07T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2205812/submissions/132286166",
+      "submitted": true
+    },
+    {
+      "title": "HW 1 (Written)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-24T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2227557/submissions/136170201",
+      "submitted": true
+    },
+    {
+      "title": "HW 1 (Code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-24T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2227561/submissions/136170263",
+      "submitted": true
+    },
+    {
+      "title": "HW 0 (Self-Grade)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-22T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2230391/submissions/132286234",
+      "submitted": true
+    },
+    {
+      "title": "HW 0 (Resubmission)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-22T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2230637/submissions/135783047",
+      "submitted": true
+    },
+    {
+      "title": "HW 1 (Self-Grade)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-25T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2247450/submissions/136170163",
+      "submitted": true
+    },
+    {
+      "title": "HW 1 (Resubmission)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-25T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2247526/submissions/136321030",
+      "submitted": true
+    },
+    {
+      "title": "Scribe Notes (Round 0, zip file with LaTeX source)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "Scribe Notes Review 1 (PDF file with comments)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "Scribe Notes (Round 1, zip file with LaTeX source)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "Scribe Notes Review 2 (PDF file with comments)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "Final Scribe Notes (Round 2, zip file with LaTeX source)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "HW 2 (Written)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-25T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2252177/submissions/135801569",
+      "submitted": true
+    },
+    {
+      "title": "HW 2 (Code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-09-25T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2252181/submissions/135801815",
+      "submitted": true
+    },
+    {
+      "title": "HW 2 (Self-Grade)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-02T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2278466/submissions/135801874",
+      "submitted": true
+    },
+    {
+      "title": "HW 2 (Resubmission)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-02T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "HW 3 (Written)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-02T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2286493/submissions/137969374",
+      "submitted": true
+    },
+    {
+      "title": "HW 3 (Code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-02T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2286500/submissions/137969171",
+      "submitted": true
+    },
+    {
+      "title": "Lecture Demo (Round 0, zip file with code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "Lecture Demo Review 1 (PDF file with comments)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "Lecture Demo (Round 1, zip file with code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "Lecture Demo Review 2 (PDF file with comments)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "Final Lecture Demo (Round 2, zip file with code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "HW 3 (Resubmission)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-10T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "HW 3 (Self-Grade)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-10T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2296649/submissions/137963869",
+      "submitted": true
+    },
+    {
+      "title": "HW 4 (Written)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-17T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2312972/submissions/141712997",
+      "submitted": true
+    },
+    {
+      "title": "HW 4 (Code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-17T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2312980/submissions/141700795",
+      "submitted": true
+    },
+    {
+      "title": "HW 5 (Code)  ",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-22T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2326822/submissions/143104432",
+      "submitted": true
+    },
+    {
+      "title": "HW 5 (Written)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-22T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2342720/submissions/143130941",
+      "submitted": true
+    },
+    {
+      "title": "HW 4 (Self-Grade)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-11-01T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2342741/submissions/141707859",
+      "submitted": true
+    },
+    {
+      "title": "HW 4 (Resubmission)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-11-01T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "HW 5 (Resubmission)  ",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-30T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "HW 5 (Self-Grade)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-30T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2355469/submissions/143105386",
+      "submitted": true
+    },
+    {
+      "title": "HW 6 (Written)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-30T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2374076/submissions/145164716",
+      "submitted": true
+    },
+    {
+      "title": "HW 6 (Code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-10-30T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2374088/submissions/145164921",
+      "submitted": true
+    },
+    {
+      "title": "HW 6 (Self-Grade)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-11-13T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603/assignments/2378514/submissions/145160978",
+      "submitted": true
+    },
+    {
+      "title": "HW 6 (Resubmission)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-11-13T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    }
+  ],
+  "CS 61C": [
+    {
+      "title": "Lab 0",
+      "course": "CS 61C",
+      "dueDate": "2022-08-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2199763/submissions/130945547",
+      "submitted": true
+    },
+    {
+      "title": "Lab 1",
+      "course": "CS 61C",
+      "dueDate": "2022-09-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2201115/submissions/131829568",
+      "submitted": true
+    },
+    {
+      "title": "Lab 2",
+      "course": "CS 61C",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2226175/submissions/132898722",
+      "submitted": true
+    },
+    {
+      "title": "Project 1",
+      "course": "CS 61C",
+      "dueDate": "2022-09-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2228379/submissions/133162214",
+      "submitted": true
+    },
+    {
+      "title": "Project 2B",
+      "course": "CS 61C",
+      "dueDate": "2022-10-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2238648/submissions/140236579",
+      "submitted": true
+    },
+    {
+      "title": "Lab 3",
+      "course": "CS 61C",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2242201/submissions/134088172",
+      "submitted": true
+    },
+    {
+      "title": "Lab 4",
+      "course": "CS 61C",
+      "dueDate": "2022-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2265396/submissions/135998488",
+      "submitted": true
+    },
+    {
+      "title": "Project 2A",
+      "course": "CS 61C",
+      "dueDate": "2022-09-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2267698/submissions/139561652",
+      "submitted": true
+    },
+    {
+      "title": "Lab 5",
+      "course": "CS 61C",
+      "dueDate": "2022-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2311452/submissions/139517837",
+      "submitted": true
+    },
+    {
+      "title": "Lab 6",
+      "course": "CS 61C",
+      "dueDate": "2022-10-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2349735/submissions/142961009",
+      "submitted": true
+    },
+    {
+      "title": "Midterm Take Home Q1",
+      "course": "CS 61C",
+      "dueDate": "2022-10-16T12:00:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2353619/submissions/141236906",
+      "submitted": true
+    },
+    {
+      "title": "Midterm Take Home Q2",
+      "course": "CS 61C",
+      "dueDate": "2022-10-16T12:00:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2353620/submissions/141239709",
+      "submitted": true
+    },
+    {
+      "title": "Midterm Take Home Q3",
+      "course": "CS 61C",
+      "dueDate": "2022-10-16T12:00:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2353621/submissions/141251056",
+      "submitted": true
+    },
+    {
+      "title": "Project 3A",
+      "course": "CS 61C",
+      "dueDate": "2022-10-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2353854/submissions/145017792",
+      "submitted": true
+    },
+    {
+      "title": "Lab 7",
+      "course": "CS 61C",
+      "dueDate": "2022-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760/assignments/2374169/submissions/144907018",
+      "submitted": true
+    },
+    {
+      "title": "Project 3B",
+      "course": "CS 61C",
+      "dueDate": "2022-11-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760",
+      "submitted": false
+    },
+    {
+      "title": "Lab 8",
+      "course": "CS 61C",
+      "dueDate": "2022-11-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760",
+      "submitted": false
+    }
+  ],
+  "EECS 106A": [
+    {
+      "title": "Syllabus Quiz (HW0)",
+      "course": "EECS 106A",
+      "dueDate": "2022-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2204639/submissions/131052095",
+      "submitted": true
+    },
+    {
+      "title": "HW0 (pdf)",
+      "course": "EECS 106A",
+      "dueDate": "2022-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2208472/submissions/131466579",
+      "submitted": true
+    },
+    {
+      "title": "HW0 (code)",
+      "course": "EECS 106A",
+      "dueDate": "2022-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2208474/submissions/131129409",
+      "submitted": true
+    },
+    {
+      "title": "SID.txt",
+      "course": "EECS 106A",
+      "dueDate": "2022-12-16T23:00:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2212516/submissions/131232550",
+      "submitted": true
+    },
+    {
+      "title": "HW1 (pdf)",
+      "course": "EECS 106A",
+      "dueDate": "2022-09-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2227514/submissions/132550927",
+      "submitted": true
+    },
+    {
+      "title": "HW1 (code)",
+      "course": "EECS 106A",
+      "dueDate": "2022-09-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2230048/submissions/132549576",
+      "submitted": true
+    },
+    {
+      "title": "Robot Usage Quiz",
+      "course": "EECS 106A",
+      "dueDate": "2022-09-23T23:00:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2248680/submissions/133229356",
+      "submitted": true
+    },
+    {
+      "title": "HW2 (pdf)",
+      "course": "EECS 106A",
+      "dueDate": "2022-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2252339/submissions/134746619",
+      "submitted": true
+    },
+    {
+      "title": "HW2 (code)",
+      "course": "EECS 106A",
+      "dueDate": "2022-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2252340/submissions/134101158",
+      "submitted": true
+    },
+    {
+      "title": "HW3 (code)",
+      "course": "EECS 106A",
+      "dueDate": "2022-09-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2258880/submissions/135370074",
+      "submitted": true
+    },
+    {
+      "title": "HW3 (pdf)",
+      "course": "EECS 106A",
+      "dueDate": "2022-09-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119",
+      "submitted": false
+    },
+    {
+      "title": "HW4",
+      "course": "EECS 106A",
+      "dueDate": "2022-09-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2288067/submissions/136581586",
+      "submitted": true
+    },
+    {
+      "title": "Post-Midterm Feedback Form I",
+      "course": "EECS 106A",
+      "dueDate": "2022-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2309779/submissions/137974412",
+      "submitted": true
+    },
+    {
+      "title": "Final Project Mini Proposal",
+      "course": "EECS 106A",
+      "dueDate": "2022-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2327986/submissions/139130279",
+      "submitted": true
+    },
+    {
+      "title": "HW5 (pdf)",
+      "course": "EECS 106A",
+      "dueDate": "2022-10-17T23:00:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2339626/submissions/141692235",
+      "submitted": true
+    },
+    {
+      "title": "HW5 (Code)",
+      "course": "EECS 106A",
+      "dueDate": "2022-10-17T23:00:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2341318/submissions/141657777",
+      "submitted": true
+    },
+    {
+      "title": "HW6",
+      "course": "EECS 106A",
+      "dueDate": "2022-10-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2361717/submissions/142644319",
+      "submitted": true
+    },
+    {
+      "title": "HW7",
+      "course": "EECS 106A",
+      "dueDate": "2022-10-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2381349/submissions/145111844",
+      "submitted": true
+    },
+    {
+      "title": "HW8 (pdf)",
+      "course": "EECS 106A",
+      "dueDate": "2022-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2388545/submissions/145667679",
+      "submitted": true
+    },
+    {
+      "title": "HW8 (code)",
+      "course": "EECS 106A",
+      "dueDate": "2022-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2388552/submissions/145666237",
+      "submitted": true
+    },
+    {
+      "title": "Final Project Proposal",
+      "course": "EECS 106A",
+      "dueDate": "2022-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/429119/assignments/2392117/submissions/144808589",
+      "submitted": true
+    }
+  ],
+  "Stat 198": [
+    {
+      "title": "Lecture 1 Quiz",
+      "course": "Stat 198",
+      "dueDate": "2022-09-14T20:09:00",
+      "link": "https://www.gradescope.com/courses/441168/assignments/2248390/submissions/133281667",
+      "submitted": true
+    },
+    {
+      "title": "Lecture 3 Quiz",
+      "course": "Stat 198",
+      "dueDate": "2022-09-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/441168",
+      "submitted": false
+    },
+    {
+      "title": "Lecture 4 Quiz",
+      "course": "Stat 198",
+      "dueDate": "2022-10-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/441168",
+      "submitted": false
+    },
+    {
+      "title": "Lecture 5 Quiz",
+      "course": "Stat 198",
+      "dueDate": "2022-10-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/441168",
+      "submitted": false
+    },
+    {
+      "title": "Lecture 6 Quiz",
+      "course": "Stat 198",
+      "dueDate": "2022-10-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/441168",
+      "submitted": false
+    }
+  ]
 };
