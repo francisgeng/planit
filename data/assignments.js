5078d2feb3801c1ed6dc0c53135928cbe8a7da27
planit <planit@tanjeffreyz.github.io>
2022-11-14 07:35:31 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -449,6 +449,13 @@ assignments = {
       "dueDate": "2022-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/423760",
       "submitted": false
+    },
+    {
+      "title": "Project 4",
+      "course": "CS 61C",
+      "dueDate": "2022-11-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/423760",
+      "submitted": false
     }
   ],
   "EECS 106A": [
