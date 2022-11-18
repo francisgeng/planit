76d0992d96046f7e97da08517bc7266ef4c8792e
planit <planit@tanjeffreyz.github.io>
2022-11-18 08:40:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -689,7 +689,7 @@ assignments = {
       "title": "Final Project Proposal",
       "course": "Stat 198",
       "dueDate": "2022-11-17T23:59:00",
-      "link": "https://www.gradescope.com/courses/441168/assignments/2433454/submissions/new",
+      "link": "https://www.gradescope.com/courses/441168",
       "submitted": false
     }
   ]
