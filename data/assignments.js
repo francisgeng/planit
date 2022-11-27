10b5af30ee613baf44b4a4ceced2c562819df654
planit <planit@tanjeffreyz.github.io>
2022-11-27 21:32:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -342,6 +342,20 @@ assignments = {
       "dueDate": "2022-11-30T23:00:00",
       "link": "https://www.gradescope.com/courses/425603/assignments/2446581/submissions/150462790",
       "submitted": true
+    },
+    {
+      "title": "HW 9 (Self-Grade)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-11T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "HW 9 (Resubmission)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-11T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
     }
   ],
   "CS 61C": [
