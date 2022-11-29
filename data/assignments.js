afcf00c4bca6b0c6cd5f1b102dbc80b3e363a634
planit <planit@tanjeffreyz.github.io>
2022-11-29 18:38:34 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -659,8 +659,8 @@ assignments = {
       "title": "Departmental Feedback Form (Post-Midterm Feedback Form II)",
       "course": "EECS 106A",
       "dueDate": "2022-12-12T23:59:00",
-      "link": "https://www.gradescope.com/courses/429119",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/429119/assignments/2458478/submissions/151249836",
+      "submitted": true
     }
   ],
   "Stat 198": [
