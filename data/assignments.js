e3d55dba024a2c055087b368d3cf6cb922d46f8f
planit <planit@tanjeffreyz.github.io>
2022-12-08 05:32:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -370,6 +370,20 @@ assignments = {
       "dueDate": "2022-12-07T23:00:00",
       "link": "https://www.gradescope.com/courses/425603",
       "submitted": false
+    },
+    {
+      "title": "HW 10 (Written)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-08T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
+    },
+    {
+      "title": "HW 10 (Code)",
+      "course": "CS 182/282A",
+      "dueDate": "2022-12-08T23:00:00",
+      "link": "https://www.gradescope.com/courses/425603",
+      "submitted": false
     }
   ],
   "CS 61C": [
